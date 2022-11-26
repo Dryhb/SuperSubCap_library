607f162511923d9b9fad08a0dfc7e85369e8f328
Dryhb <66953926+Dryhb@users.noreply.github.com>
2022-11-26 22:32:36 +0100

    fix mapping upgrader

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
   name='SuperSubCap',
-  version='0.0.5',
+  version='0.0.6',
   description='Superscripts and subscripts for latin and greek letters, numbers and operands',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='https://github.com/Dryhb/SuperSubCap_library',  
